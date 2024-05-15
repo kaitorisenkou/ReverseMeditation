bbf4531e3303cd8f7b9413f2fdc61a8a9f5bf20e
kaitorisenkou <kaitorisenkou@icloud.com>
2024-05-15 21:19:31 +0900

    プロジェクト ファイルを追加します。

---
+++ ReverseMeditation/Building_ReverseMeditationDebug.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Verse;
+using RimWorld;
+using Verse.AI;
+using Unity.Jobs;
+using RimWorld.Planet;
+
+namespace ReverseMeditation {
+    [DefOf]
+    public static class RMDefOf {
+        static RMDefOf() {
+            DefOfHelper.EnsureInitializedInCtor(typeof(RMDefOf));
+        }
+        [MayRequireRoyalty]
+        public static JobDef ReverseMeditate;
+    }
+    public class Building_ReverseMeditationDebug : Building {
+        public override IEnumerable<FloatMenuOption> GetFloatMenuOptions(Pawn selPawn) {
+            foreach (FloatMenuOption floatMenuOption in base.GetFloatMenuOptions(selPawn)) {
+                yield return floatMenuOption;
+            }
+            var targ = new LocalTargetInfo(this);
+            if (targ.IsValid && selPawn.CanReach(this, PathEndMode.OnCell, selPawn.NormalMaxDanger())) {
+                JobDef def1 = JobDefOf.Meditate;
+                var focus = MeditationUtility.BestFocusAt(targ, selPawn);
+                var job1 = JobMaker.MakeJob(def1, targ, null, focus);
+                job1.ignoreJoyTimeAssignment = true;
+                yield return new FloatMenuOption(
+                    "meditate",
+                    delegate () {
+                        selPawn.jobs.TryTakeOrderedJob(job1, new JobTag?(JobTag.Misc), false);
+                    });
+
+                JobDef def2 = RMDefOf.ReverseMeditate;
+                var job2 = JobMaker.MakeJob(def2, targ, null, focus);
+                job2.ignoreJoyTimeAssignment = true;
+                yield return new FloatMenuOption(
+                    "reverse meditate",
+                    delegate () {
+                        selPawn.jobs.TryTakeOrderedJob(job2, new JobTag?(JobTag.Misc), false);
+                    });
+            }
+        }
+    }
+}
---
+++ ReverseMeditation/JobDriver_ReverseMeditate.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections.Generic;
+
+using Verse;
+using RimWorld;
+using Verse.AI;
+using UnityEngine;
+using Verse.Sound;
+
+namespace ReverseMeditation {
+    public class JobDriver_ReverseMeditate : JobDriver_Meditate {
+        private const int JobCheckIntervalTicks = 18000;
+        private Mote psyfocusMote;
+        private bool FromBed {
+            get {
+                return job.GetTarget(TargetIndex.B).IsValid;
+            }
+        }
+
+        //ベースからコピペ
+        protected override IEnumerable<Toil> MakeNewToils() {
+            Toil meditate = ToilMaker.MakeToil("MakeNewToils");
+            meditate.socialMode = RandomSocialMode.Off;
+            if (FromBed) {
+                this.KeepLyingDown(TargetIndex.B);
+                meditate = Toils_LayDown.LayDown(TargetIndex.B, job.GetTarget(TargetIndex.B).Thing is Building_Bed, false, false, true, PawnPosture.LayingOnGroundNormal, false);
+            } else {
+                yield return Toils_Goto.GotoCell(TargetIndex.A, PathEndMode.OnCell);
+                meditate.initAction = delegate () {
+                    LocalTargetInfo target = job.GetTarget(TargetIndex.C);
+                    if (target.IsValid) {
+                        faceDir = target.Cell - pawn.Position;
+                        return;
+                    }
+                    faceDir = (job.def.faceDir.IsValid ? job.def.faceDir : Rot4.Random).FacingCell;
+                };
+                if (Focus != null) {
+                    meditate.FailOnDespawnedOrNull(TargetIndex.C);
+                    meditate.FailOnForbidden(TargetIndex.A);
+                    if (pawn.HasPsylink && Focus.Thing != null) {
+                        meditate.FailOn(() => Focus.Thing.GetStatValueForPawn(StatDefOf.MeditationFocusStrength, pawn, true) < float.Epsilon);
+                    }
+                }
+                meditate.handlingFacing = true;
+            }
+            meditate.defaultCompleteMode = ToilCompleteMode.Delay;
+            //meditate.defaultDuration = job.def.joyDuration;
+            meditate.FailOn(() => !MeditationUtility.CanMeditateNow(pawn) || !MeditationUtility.SafeEnvironmentalConditions(pawn, TargetLocA, Map));
+            meditate.AddPreTickAction(delegate {
+                var assignment = pawn.GetTimeAssignment();
+                bool flag = assignment == TimeAssignmentDefOf.Meditate|| assignment== TimeAssignmentDefOf.Anything;
+                if (job.ignoreJoyTimeAssignment) {
+                    Pawn_PsychicEntropyTracker psychicEntropy = pawn.psychicEntropy;
+                    bool flag2 = !flag && job.wasOnMeditationTimeAssignment;
+                    if (pawn.IsHashIntervalTick(JobCheckIntervalTicks) && psychicEntropy != null && !flag && psychicEntropy.CurrentPsyfocus <= Mathf.Max(psychicEntropy.TargetPsyfocus - 0.05f, 0f)) {
+                        pawn.jobs.CheckForJobOverride(0f);
+                        return;
+                    }
+                    if (flag2 && psychicEntropy.TargetPsyfocus > psychicEntropy.CurrentPsyfocus) {
+                        base.EndJobWith(JobCondition.InterruptForced);
+                        return;
+                    }
+                    job.psyfocusTargetLast = psychicEntropy.TargetPsyfocus;
+                    job.wasOnMeditationTimeAssignment = flag;
+                } else if (pawn.needs.joy.CurLevelPercentage >= 1f) {
+                    EndJobWith(JobCondition.InterruptForced);
+                    return;
+                }
+                if (faceDir.IsValid && !FromBed) {
+                    pawn.rotationTracker.FaceCell(pawn.Position + faceDir);
+                }
+                ReverseMeditationTick();
+                if (ModsConfig.RoyaltyActive && MeditationFocusDefOf.Natural.CanPawnUse(pawn)) {
+                    int num = GenRadial.NumCellsInRadius(MeditationUtility.FocusObjectSearchRadius);
+                    for (int i = 0; i < num; i++) {
+                        IntVec3 c = pawn.Position + GenRadial.RadialPattern[i];
+                        if (c.InBounds(pawn.Map)) {
+                            Plant plant = c.GetPlant(pawn.Map);
+                            if (plant != null && plant.def == ThingDefOf.Plant_TreeAnima) {
+                                CompSpawnSubplant compSpawnSubplant = plant.TryGetComp<CompSpawnSubplant>();
+                                if (compSpawnSubplant != null) {
+                                    compSpawnSubplant.AddProgress(JobDriver_Meditate.AnimaTreeSubplantProgressPerTick, false);
+                                }
+                            }
+                        }
+                    }
+                }
+            });
+            yield return meditate;
+            yield break;
+        }
+
+        //ベースのMeditationTick()から大部分をコピペ
+        private void ReverseMeditationTick() {
+            pawn.skills.Learn(SkillDefOf.Intellectual, 0.018000001f, false, false);
+            pawn.GainComfortFromCellIfPossible(false);
+            /*
+            if (pawn.needs.joy != null) {
+                JoyUtility.JoyTickCheckEnd(pawn, JoyTickFullJoyAction.None, 1f, null);
+            }
+            */
+            if (pawn.IsHashIntervalTick(100)) {
+                FleckMaker.ThrowMetaIcon(pawn.Position, pawn.Map, FleckDefOf.Meditating, 0.42f);
+            }
+            if (ModsConfig.RoyaltyActive && pawn.psychicEntropy != null) {
+                pawn.psychicEntropy.Notify_Meditated();
+                if (pawn.HasPsylink && pawn.psychicEntropy.PsychicSensitivity > 1E-45f) {
+                    float yOffset = (float)(pawn.Position.x % 2 + pawn.Position.z % 2) / 10f;
+                    if (psyfocusMote == null || psyfocusMote.Destroyed) {
+                        psyfocusMote = MoteMaker.MakeAttachedOverlay(pawn, ThingDefOf.Mote_PsyfocusPulse, Vector3.zero, 1f, -1f);
+                        psyfocusMote.yOffset = yOffset;
+                    }
+                    psyfocusMote.Maintain();
+                    if (sustainer == null || sustainer.Ended) {
+                        sustainer = SoundDefOf.MeditationGainPsyfocus.TrySpawnSustainer(SoundInfo.InMap(pawn, MaintenanceType.PerTick));
+                    }
+                    sustainer.Maintain();
+                    //pawn.psychicEntropy.GainPsyfocus(Focus.Thing);
+                    var focusGain = MeditationUtility.PsyfocusGainPerTick(pawn, Focus.Thing);
+                    pawn.psychicEntropy.OffsetPsyfocusDirectly(-focusGain);
+                }
+            }
+        }
+    }
+}
